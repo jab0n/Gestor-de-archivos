336576843f8baff7976a08815caf521416137681
unknown <jab0n@outlook.com>
2025-09-28 20:58:16 -0600

    V1.0

--- uploads/views.py
+++ uploads/views.py
@@ -3,7 +3,7 @@ from .forms import MultipleFileUploadForm
 from .models import Archivo
 
 def subir_archivos(request):
-    if request.FILES.getlist('field_name'):
+    if request.method == "POST":
         form = MultipleFileUploadForm(request.POST, request.FILES)
         if form.is_valid():
             archivos = form.cleaned_data["archivos"]
@@ -14,6 +14,7 @@ def subir_archivos(request):
         form = MultipleFileUploadForm()
 
     return render(request, "uploads/subir.html", {"form": form})
+
 
 def lista_archivos(request):
     archivos = Archivo.objects.all()
