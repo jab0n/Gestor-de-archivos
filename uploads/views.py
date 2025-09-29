82c4a432362d9c2c08fca0369b38db1c98bb9e05
unknown <jab0n@outlook.com>
2025-09-28 20:07:27 -0600

    V0.6

--- uploads/views.py
+++ uploads/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import MultipleFileUploadForm
+from .models import Archivo
 
-# Create your views here.
+def subir_archivos(request):
+    if request.method == "POST":
+        form = MultipleFileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            archivos = form.cleaned_data["archivos"]
+            for archivo in archivos:
+                Archivo.objects.create(archivo=archivo)
+            return redirect("lista_archivos")
+    else:
+        form = MultipleFileUploadForm()
+
+    return render(request, "uploads/subir.html", {"form": form})
+
+def lista_archivos(request):
+    archivos = Archivo.objects.all()
+    return render(request, "uploads/lista.html", {"archivos": archivos})
